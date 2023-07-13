d7cbf056718139e69663f4037ec8c3540cfaa8a2
arvindkeshri <arvidce@gmaiol.com>
2023-07-14 00:19:03 +0530

    commit 1

--- dom.js
+++ dom.js
@@ -27,9 +27,9 @@
 //header.style.borderBottom = 'solid 3px #000';
 
 //get element by class name
-var items = document.getElementsByClassName('list-group-item');
+items[1].var items = document.getElementsByClassName('list-group-item');
 console.log(items[1]);
-items[1].textContent = 'Hello 2';
+textContent = 'Hello 2';
 items[1].style.fontWeight = 'bold';
 items[1].style.backgroundColor = 'yellow';
 
@@ -40,6 +40,20 @@ for(var i=0; i<items.length; i++){
 }
 
 //get elements by tag name
+
+items[1].var items = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent = 'Hello 2';
+li[1].style.fontWeight = 'bold';
+li[1].style.backgroundColor = 'yellow';
+
+for(var i=0; i<li.length; i++){
+    li[i].style.backgroundColor = '#f4f4f4';
+}
+
+
+
 
 
 
